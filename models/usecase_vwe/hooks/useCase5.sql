464622c7c0e9249108348dd60155763add4fdb0b
hemashree.shrivastava@techment.com <hemashree.shrivastava@techment.com>
2022-08-05 09:02:09 +0000

    commit

---
+++ models/usecase_vwe/hooks/useCase5.sql
@@ -0,0 +1,16 @@
+{{ config(
+    pre_hook=[
+      "ALTER TABLE VWE2.PUBLIC.PREMIUM_CUSTOMER_RESULT ADD MEMBERSHIP_PLAN VARCHAR"
+    ],
+    post_hook=[
+      "update  VWE2.PUBLIC.PREMIUM_CUSTOMER_RESULT  set MEMBERSHIP_PLAN =
+                case 
+                when MEMBERSHIP_STATUS = 'Active' and TENURE_IN_MONTHS >=36 then 'Gold'
+                when MEMBERSHIP_STATUS = 'Active' and TENURE_IN_MONTHS >=19 And TENURE_IN_MONTHS <36 then 'Silver'
+                when  MEMBERSHIP_STATUS = 'Active' and TENURE_IN_MONTHS <=18 then 'Gold'
+                end"
+    ]
+) }}
+
+select * from VWE2.PUBLIC.ORDER_TABLE
+
